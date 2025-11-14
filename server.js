acda63d220f7a52a807ac5828193e7b4ade66f23
frankzhang <frank.zhang.sa.au@gmail.com>
2025-11-14 13:40:05 +1000

    1. Add dev Dependencies: nodemon, so when change code, server will auto re-start

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
+// Address of this server: 
+// URL -> http://localhost:8383
+// IP -> 127.0.0.1:8383
 import express from "express"
 
 const app = express()
 const PORT = 8383
+console.log(`Server has not started 123`)
 app.listen(PORT, () => {
     console.log(`Server has started on: ${PORT}`)
 })

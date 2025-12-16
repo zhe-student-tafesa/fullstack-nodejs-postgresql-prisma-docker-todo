1808cb84750cca42d6301d06b3f1403178183ffb
frankzhang <frank.zhang.sa.au@gmail.com>
2025-12-16 14:07:18 +1000

    1. Create DB tables;

---
+++ src/db.js
@@ -0,0 +1,23 @@
+import { DatabaseSync } from 'node:sqlite'
+const db = new DatabaseSync(':memory')
+
+// Exccute SQL statements from strings
+db.exec(`
+    CREATE TABLE users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT UNIQUE,
+        password TEXT
+    )
+`)
+
+db.exec(`
+    CREATE TABLE todos (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user_id INTEGER,
+        task TEXT,
+        completed BOOLEAN DEFAULT 0,
+        FOREIGN KEY(user_id) REFERENCES users(id)
+    )
+`)
+
+export default db;
--- src/server.js
+++ src/server.js
@@ -8,14 +8,14 @@ import { fileURLToPath } from "url"
 const app = express()
 const PORT = process.env.PORT || 5003
 
-// use middleware to config SERVER
-app.use(express.json())
 
 // Get the file path from the URL of the current module
 const __filename = fileURLToPath(import.meta.url)
 // Get the directory name from the file path
 const __dirname = dirname(__filename)
 
+// use Middleware to config SERVER
+app.use(express.json())
 // Serves the HTML file from the /public directory
 // Tells express to serve all files from the public folder as static assets / file. Any requests for the css files will be resolved to the public directory.
 app.use(express.static(path.join(__dirname, '../public')))

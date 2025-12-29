d08633a0d7dbb288c6911ae856da5ab64c13a313
frankzhang <frank.zhang.sa.au@gmail.com>
2025-12-29 12:06:06 +1000

    Finish get todo api

--- src/routes/todoRoutes.js
+++ src/routes/todoRoutes.js
@@ -1,11 +1,13 @@
 import express from 'express'
+import db from '../db.js'
 
 const router = express.Router()
 
 // Get all todos for logged-in user
 router.get('/', (req, res) => {
-    res.sendStatus(200)
-
+    const getTodos = db.prepare('SELECT * FROM todos WHERE user_id = ?')
+    const todos = getTodos.all(req.userId)
+    res.json(todos)
 })
 
 // Create a new todo
